c54d33444d330927b22e483a0b49da6a393444e1
TANAKA0710 <tomo-tanaka@sr-net.co.jp>
2017-07-14 14:30:03 +0900

    update
    update

--- Jenkins_sh/commit.sh
+++ Jenkins_sh/commit.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-TODAY=`date '+%F'`
-cd C:/Users/SR031970/Desktop/remote_clone
+cd C:\Users\SR031970\Desktop\remote_clone
 git add .
-git commit -m "commit" $TODAY
+git commit -m "commit" 
